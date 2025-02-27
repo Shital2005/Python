0100b126c952f1872ef148e5b52c0e3282542dbf
Shital2005 <shitalvijayprajapati85@gmail.com>
2025-02-27 14:08:47 +0530

    constructor overriding

---
+++ Session3/construct_Ex.py
@@ -0,0 +1,37 @@
+# class Rbi:
+#     def homeloan_ROI(self):
+#         print("8%")
+#     def carloan(self):
+#         print("10%")
+# class Sbi(Rbi):
+#     def homeloan_ROI(self):
+#         print("7%")
+# obj = Sbi()
+# obj.homeloan_ROI()
+# obj.carloan()            
+
+
+# class Rbi:
+#     def homeloan_ROI(self):
+#         print("8%")
+#     def carloan(self):
+#         print("10%")
+# class Sbi(Rbi):
+#     def homeloan_ROI(self):
+#         print("7%")
+#         super().homeloan_ROI()
+# obj = Sbi()
+# obj.homeloan_ROI()
+# obj.carloan()            
+
+
+
+# constructor overriding
+
+class Father:
+    def __init__(self):
+        print("Father class constructor")
+class Son(Father):
+    def __init__(self):
+        print("Son class constructor")
+obj = Son()
