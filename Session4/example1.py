966741a929cdd1c41e59a5cf66889bdbb8115e06
Shital2005 <shitalvijayprajapati85@gmail.com>
2025-02-28 10:39:46 +0530

    static method

--- Session4/example1.py
+++ Session4/example1.py
@@ -8,6 +8,17 @@ class Person:
 person1 = Person('Shital')  
 person1.greet()    
 
+class Student(Person):
+    def __init__(self,name,student_id):
+        super().__init__(name)
+        self.student_id = student_id
+    @staticmethod
+    def welcome(name,student_id):
+        print(f"Welcome, {name} , {student_id}")
+
+student1 = Student('Shital', 1234) 
+Student.welcome("Shital", 1234)       
+
 
 
 
